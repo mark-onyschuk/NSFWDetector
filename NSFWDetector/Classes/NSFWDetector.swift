245d96d5be222daa580eb0493d5221e948381a6b
Mark Onyschuk <mark.onyschuk@ndimensionl.com>
2024-10-07 06:00:22 -0500

    Replace NSWF.mlmodel with OpenNSFW

--- NSFWDetector/Classes/NSFWDetector.swift
+++ NSFWDetector/Classes/NSFWDetector.swift
@@ -29,7 +29,7 @@ public class NSFWDetector {
     private let model: VNCoreMLModel
 
     public required init() {
-        guard let model = try? VNCoreMLModel(for: NSFW(configuration: MLModelConfiguration()).model) else {
+        guard let model = try? VNCoreMLModel(for: OpenNSFW(configuration: MLModelConfiguration()).model) else {
             fatalError("NSFW should always be a valid model")
         }
         self.model = model
